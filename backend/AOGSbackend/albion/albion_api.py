77484ae2d5c666b638cafb786ecbfd00e74dfb89
Maxime L'Hermite <maxime.lhermite@epita.fr>
2020-04-03 13:17:15 +0200

    [BACK] Fix removing unexisting import

--- backend/AOGSbackend/albion/albion_api.py
+++ backend/AOGSbackend/albion/albion_api.py
@@ -1,17 +1,14 @@
-from typing import Optional, List, Tuple
+from typing import Optional, Tuple
 
 import requests
-from jsonschema import Draft7Validator
 from requests import Response
-from typing_extensions import Final
 
 from AOGSbackend.albion.albion_api_models import AlbionApiSearchElement, AlbionApiMinimalPlayer
 from AOGSbackend.utils.url_tools import url_join
 
 
 class AlbionApi:
-
-    BASE_URL: Final[str] = "https://albiononline.com/api/gameinfo/"
+    BASE_URL: str = "https://albiononline.com/api/gameinfo/"
 
     @staticmethod
     def get_player_by_name(username: str) -> Optional[AlbionApiMinimalPlayer]:
